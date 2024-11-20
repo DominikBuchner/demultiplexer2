32d8392d94025429696bbd34eeb5e2003938e18c
DominikBuchner <38790188+DominikBuchner@users.noreply.github.com>
2024-11-20 15:26:44 +0100

    update

---
+++ demultiplexer2/create_primerset.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import pkg_resources, datetime
+from pathlib import Path
+
+
+def create_primerset(primerset_name: str, n_primers: int):
+    """Function to create a primerset in an Excel sheet.
+
+    Args:
+        primerset_name (str): Name of the primerset.
+        n_primers (int): Number of primers in the primerset.
+    """
+    # generate a path to save the primerset. This is wherever demultiplexer2 is installed.
+    primerset_savepath = Path(
+        pkg_resources.resource_filename(__name__, "data/primersets/")
+    ).joinpath("{}_primerset.xlsx".format(primerset_name))
+
+    if primerset_savepath.is_file():
+        print(
+            "{}: A primerset with this name already exists. Please choose another name.".format(
+                datetime.datetime.now().strftime("%H:%M:%S")
+            )
+        )
+        return None
+
+    # write output to excel
+    with pd.ExcelWriter(primerset_savepath, mode="w", engine="openpyxl") as writer:
+        # create the general information
+        general_information = pd.DataFrame(
+            [
+                [
+                    "Put the forward primer used for amplification here.",
+                    "Put the reverse primer used for amplification here.",
+                ]
+            ],
+            columns=["Forward primer (5' - 3')", "Reverse primer (5' - 3')"],
+        )
+
+        general_information.to_excel(
+            writer, sheet_name="general_information", index=False
+        )
+
+        # create the forward sets
+        forward_tags = pd.DataFrame(
+            [["", ""]] * n_primers, columns=["name", "sequence"]
+        )
+        forward_tags.index = forward_tags.index + 1
+
+        # since forward and reverse tag sheets are identical they can be written from the same dataset
+        for sheet_name in ["forward_tags", "reverse_tags"]:
+            forward_tags.to_excel(writer, sheet_name=sheet_name, index=True)
+
+    print(
+        "{}: Primerset was saved at:\n{}.".format(
+            datetime.datetime.now().strftime("%H:%M:%S"), primerset_savepath
+        )
+    )
+
+
+create_primerset("test", 6)
