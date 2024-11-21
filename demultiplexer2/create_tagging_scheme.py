ac0e44581e6adf1434c315be6c0659e11a7fd2d4
DominikBuchner <38790188+DominikBuchner@users.noreply.github.com>
2024-11-21 09:45:51 +0100

    update

--- demultiplexer2/create_tagging_scheme.py
+++ demultiplexer2/create_tagging_scheme.py
@@ -1,10 +1,46 @@
 import glob, datetime
+import pandas as pd
 from demultiplexer2 import find_file_pairs
 from pathlib import Path
 
 
+def collect_primerset_information(primerset_path: str) -> object:
+    """Function to parse the primerset file and return as a dataframe to chose from.
+    Args:
+        primerset_path (str): Path to the primerset file to be used.
+
+    Returns:
+        object: Formatted primerset for user output.
+    """
+    # parse the specific primers first
+    try:
+        general_information = pd.read_excel(
+            primerset_path, sheet_name="general_information"
+        )
+    except (FileNotFoundError, ValueError):
+        print(
+            "{}: Please select a valid input for the primerset path.".format(
+                datetime.datetime.now().strftime("%H:%M:%S")
+            )
+        )
+
+        return None
+
+    forward_primer = general_information["Forward primer (5' - 3')"].item()
+    reverse_primer = general_information["Reverse primer (5' - 3')"].item()
+
+
 def main(tagging_scheme_name: str, data_dir: str, primerset_path: str) -> None:
-    # TODO
+    """Main function to create a tagging scheme.
+
+    Args:
+        tagging_scheme_name (str): The name of the tagging scheme that is used for saving it.
+        data_dir (str): The directory where the files to demultiplex are stored.
+        primerset_path (str): The path to the primerset to use for demultiplexing.
+
+    Returns:
+        _type_: _description_
+    """
     # collect all file pairs from input folder, give warning if unpaired files are found
     all_files = glob.glob(str(Path(data_dir).joinpath("*.fastq.gz")))
     file_pairs, singles = find_file_pairs.find_file_pairs(all_files)
@@ -32,5 +68,6 @@ def main(tagging_scheme_name: str, data_dir: str, primerset_path: str) -> None:
         return None
 
     # collect primers / tags / names from primer set
+    primerset_information = collect_primerset_information(Path(primerset_path))
     # request input for combinations (form?)
     pass
