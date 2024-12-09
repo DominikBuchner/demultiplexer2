348b57781b12fcf8c8117cdbaca069c5904c3100
DominikBuchner <38790188+DominikBuchner@users.noreply.github.com>
2024-12-09 10:53:10 +0100

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="demultiplexer2",
-    version="1.1.2",
+    version="1.1.3",
     author="Dominik Buchner",
     author_email="dominik.buchner@uni-due.de",
     description="A python command line interface to demultiplex illumina reads.",
